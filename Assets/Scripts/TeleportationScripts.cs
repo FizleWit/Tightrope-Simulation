f0b0ece393cf61794c36c1076200cb5398d12151
Amine Sabil <77703578+FizleWit@users.noreply.github.com>
2022-10-27 19:02:05 -0400

    added falling constraints
    will teleport when falling now

--- Assets/Scripts/TeleportationScripts.cs
+++ Assets/Scripts/TeleportationScripts.cs
@@ -22,13 +22,15 @@ public class TeleportationScripts : MonoBehaviour
 
 
         Vector3 playerLocation = gameObject.transform.position;
-        //if Y is below a certain limit( 10 )wait 5-10 seconds and teleport to home base
+        
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
             TeleportPlayer(HomeBaseLocation);
         }
         //if y is above a certain limit (26) and z is above a certain limit (29)= made it to other side trigger something
-
+        //if Y is below a certain limit( 10 ) teleport to home base
+         if( transform.position.y <= 10f)
+            TeleportPlayer(HomeBaseLocation);
     }
 
     void TeleportPlayer(Vector3 tpLocation)
